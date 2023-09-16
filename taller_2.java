cafdbde82f35f0520eccf7388408676936606a00
andrey <andrey.murilloo@gmail.com>
2023-09-15 23:40:44 -0500

    operaciones basicas

--- taller_2.java
+++ taller_2.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class taller_2 {
     public static void main(String[] args) {
 
-        System.out.println("probando rama1");
+        //ejercicio simple con las 4 operaciones y dos numeros solicitados por teclado
 
-        
+        Scanner scanner = new Scanner(System.in);
+        int n1,n2;
+        System.out.println("Ingrese numero 1");
+        n1 = scanner.nextInt();
+        System.out.println("Ingrese numero 2");
+        n2 = scanner.nextInt();
+
+        System.out.println("la suma es "+(n1+n2));
+        System.out.println("la resta es "+(n1-n2));
+        System.out.println("la multiplicacion es "+(n1*n2));
+        System.out.println("la division es "+(n1/n2));
 
     }
 }
