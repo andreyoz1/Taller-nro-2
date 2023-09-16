69d6dc05dadbc7f2e460a9d27347778aa9b1bf8d
andrey <andrey.murilloo@gmail.com>
2023-09-15 23:56:53 -0500

    usuario y contrasena

--- taller_2.java
+++ taller_2.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class taller_2 {
     public static void main(String[] args) {
-        System.out.println("hola");
+         final String USER = "admin";
+        String myUser;
+        String userPassword;
+        byte userAge;
+        final byte MIN_AGE=18;
+
+    
+        final String PASSWORD = "12345";
+        
+
+        Scanner myscanner = new Scanner(System.in);
+      
+
+        do {
+            System.out.println("Ingrese el usuario");
+            myUser=myscanner.nextLine();
+
+            System.out.println("Ingrese la contraseña");
+            userPassword=myscanner.nextLine();
+
+        } while ((!myUser.equals(USER))&&(!userPassword.equals(PASSWORD)));        
+        myscanner.close();
+
     }
 }
